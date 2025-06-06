6b451f7de5cc96bf32a81822fb1430c4fe8a6226
Achilleas Villiotis <axillvs@gmail.com>
2025-06-06 14:28:11 +0300

    fixed activate sdk bug

--- activate_sdk_6.0.1.sh
+++ activate_sdk_6.0.1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ~/STM32MPU_Workspace/STM32MPU_Ecosystem_6.0.1/Developer-Package/SDK/environment-setup-cortexa35-ostl-linux
-export SYSROOT="~/STM32MPU_Workspace/STM32MPU_Ecosystem_6.0.1/Developer-Package/SDK/sysroots/cortexa35-ostl-linux"
+export SYSROOT="$HOME/STM32MPU_Workspace/STM32MPU_Ecosystem_6.0.1/Developer-Package/SDK/sysroots/cortexa35-ostl-linux"
